cabf8dea97b0c57896a256d622c1f5a587563872
AyselMammadova <36525005+AyselMammadova@users.noreply.github.com>
2022-09-17 16:58:25 +0000

    Deploying to gh-pages from @ AyselMammadova/NextJs-TypeScript-Tree-System@2d7d503f8dec8e3d1f4f0e17b74ef8b13f0184b9 🚀

---
+++ _next/static/k61GKbGfjWXQ9AjGwXyf1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/754-606dad4836662863.js","static/chunks/pages/index-289a91ddd6e2e9dd.js"],"/_error":["static/chunks/pages/_error-fb68742d3cf986b6.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
